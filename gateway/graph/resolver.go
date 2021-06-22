c63385a0ea968af59b98fd4fbb20359944154128
404 <ivan@test.it>
2021-06-22 17:13:23 +0300

    gql implemented

---
+++ gateway/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/rodkevich/atm/gateway/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
