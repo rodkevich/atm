087dce9127131328de089b01d747853c7080507f
404 <ivan@test.it>
2021-06-30 18:47:52 +0300

    upd: ES init

--- cmd/persons/main.go
+++ cmd/persons/main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/rodkevich/atm/persons"
+	"github.com/rodkevich/profile/repository"
+	"github.com/rodkevich/profile/repository/postgres"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
 	s := grpc.NewServer()
 	srv := &persons.GRPCServer{}
 	persons.RegisterPersonsServer(s, srv)
-
 	l, err := net.Listen("tcp", ":9090")
+
+	err = initDataBase(err)
+
 	fmt.Println("Server is running")
 	if err != nil {
 		log.Fatal(err)
@@ -21,4 +26,75 @@ func main() {
 	if err := s.Serve(l); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initDataBase(err error) error {
+	_ = os.Setenv("DATABASE_URL", "postgresql://postgres:postgres@0.0.0.0:5432/postgres")
+
+	var (
+		rep repository.Repository
+		//err error
+	)
+
+	rep, err = postgres.NewRepository() //init postgres rep db
+
+	if err != nil {
+		panic(err)
+	}
+
+	//create tables if not presented
+	err = rep.Up()
+
+	//personsList list
+	var personsList []*repository.PersonModel
+
+	//some test personsList
+	var Peter = &repository.PersonModel{
+		ProjectID:   "S0me_pr0j3ct_1d",
+		ProjectEnv:  "S0me_pr0j3ct_3nv",
+		CompanyID:   "S0me_c0mp4ny_1d",
+		TeamID:      "S0me_t34m_1d",
+		GroupID:     "S0me_gr0up_1d",
+		FirstName:   "Peter",
+		LastName:    "Bar",
+		Email:       "Peter@bar.foo",
+		Phone:       "08123456789",
+		Description: "Some Description 1",
+	}
+
+	var John = &repository.PersonModel{
+		ProjectID:   "S0me_pr0j3ct_1d",
+		ProjectEnv:  "S0me_pr0j3ct_3nv",
+		CompanyID:   "S0me_c0mp4ny_1d",
+		TeamID:      "S0me_t34m_1d",
+		GroupID:     "S0me_gr0up_2d",
+		FirstName:   "John",
+		LastName:    "Foo",
+		Email:       "John@foo.bar",
+		Phone:       "9887654210",
+		Description: "Some Description 2",
+	}
+
+	//append to list
+	personsList = append(personsList, Peter)
+	personsList = append(personsList, John)
+
+	//create users
+	for _, person := range personsList {
+		id, _ := rep.Create(person)
+		log.Println(id)
+	}
+
+	// search for users
+	users, err := rep.Find()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(users)
+
+	if err != nil {
+		panic(err)
+	}
+	return err
+
 }
---
+++ elkpersons/go.mod
@@ -0,0 +1,5 @@
+module github.com/rodkevich/atm/elkpersons
+
+go 1.16
+
+require github.com/elastic/go-elasticsearch/v8 v8.0.0-20210608143047-aa1301e7ba9d
